1ef5fd70e5065950e78c781e2dd3e53a2825c5da
StevanFreeborn <=>
2023-03-25 21:44:53 -0500

    feat: implement getComputerChoice function

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log("hello");
+function getComputerChoice() {
+  const choices = [
+    'Rock',
+    'Paper',
+    'Scissors',
+  ];
+
+  const randomIndex = Math.floor(
+    Math.random() * choices.length
+  )
+
+  return choices[randomIndex];
+}
